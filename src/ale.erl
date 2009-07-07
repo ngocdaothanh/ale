79a184e5afa84802426d2ba4186f1a85b8e06b90
Ngoc Dao <ngocdaothanh@gmail.com>
2009-07-07 23:19:57 +0900

    Add page caching using Merle

--- src/ale.erl
+++ src/ale.erl
@@ -8,51 +8,52 @@ sync() -> make:all([load]).
 
 %-------------------------------------------------------------------------------
 
-%% Each request has its own processing process. If you want to share variables
-%% across functions, you can put them to the process dictionary using put/3-5
-%% and get/1, 2.
-put(Namespace, Key, Value)                  -> erlang:put(?KEY(Namespace, Key), Value).
-put(Namespace, Key, Value1, Value2)         -> erlang:put(?KEY(Namespace, Key), {Value1, Value2}).
-put(Namespace, Key, Value1, Value2, Value3) -> erlang:put(?KEY(Namespace, Key), {Value1, Value2, Value3}).
+yaws(Key, Value) -> erlang:put(?KEY(yaws, Key), Value).
+yaws(Key, Value1, Value2) -> erlang:put(?KEY(yaws, Key), {Value1, Value2}).
+yaws(Key, Value1, Value2, Value3) -> erlang:put(?KEY(yaws, Key), {Value1, Value2, Value3}).
+yaws(Key) -> erlang:get(?KEY(yaws, Key)).
 
-get(Namespace, Key) -> erlang:get(?KEY(Namespace, Key)).
-
-get(Namespace) ->
+%% Returns the list to be sent to Yaws as the result of Yaws' out/1.
+yaws() ->
     lists:foldr(
         fun
-            ({{Namespace2, Key}, {Value1, Value2, Value3}}, Acc)
-            when Namespace2 == Namespace -> [{Key, Value1, Value2, Value3} | Acc];
-
-            ({{Namespace2, Key}, {Value1, Value2        }}, Acc)
-            when Namespace2 == Namespace -> [{Key, Value1, Value2        } | Acc];
-
-            ({{Namespace2, Key},  Value                  }, Acc)
-            when Namespace2 == Namespace -> [{Key, Value}                  | Acc];
-
-            (_                                            , Acc)
-                                         ->                                  Acc
+            ({{yaws, Key}, {Value1, Value2, Value3}}, Acc) -> [{Key, Value1, Value2, Value3} | Acc];
+            ({{yaws, Key}, {Value1, Value2        }}, Acc) -> [{Key, Value1, Value2        } | Acc];
+            ({{yaws, Key},  Value                  }, Acc) -> [{Key, Value}                  | Acc];
+            (_                                      , Acc) ->                                  Acc
         end,
         [],
         erlang:get()
     ).
 
-erase(Namespace, Key) -> erlang:erase(?KEY(Namespace, Key)).
+%% Each request has its own processing process. If you want to share variables
+%% across functions, use app/2 and app/1.
+app(Key, Value) -> erlang:put(?KEY(app, Key), Value).
+app(Key) -> erlang:get(?KEY(app, Key)).
 
 %-------------------------------------------------------------------------------
 
-content_for_layout() ->
-    ale:get(ale, content_for_layout).
+layout(Value) -> ale(layout, Value).
+layout()      -> ale(layout).
 
+view(Value) -> ale(view, Value).
+view()      -> ale(view).
+
+content_for_layout(Value) -> ale(content_for_layout, Value).
+content_for_layout()      -> ale:ale(content_for_layout).
+
+%% Accumulates Script.
 script(Script) ->
-    case get(ale, script) of
-        undefined -> put(ale, script, Script);
-        IoList    -> put(ale, script, [IoList, Script])
+    case ale(script) of
+        undefined -> ale(script, Script);
+        IoList    -> ale(script, [IoList, Script])
     end.
 
+%% Returns {script, [{type, "text/javascript"}], accumulated scripts} or "" if there is no script.
 script() ->
-    case get(ale, script) of
+    case ale(script) of
         undefined -> "";
-        IoList    -> IoList
+        IoList    -> {script, [{type, "text/javascript"}], IoList}
     end.
 
 %-------------------------------------------------------------------------------
@@ -73,3 +74,23 @@ gravatar(Email, Size) ->
     end,
     Src = io_lib:format("http://www.gravatar.com/avatar.php?size=~p&gravatar_id=~s", [Size, GravatarId]),
     {img, [{src, Src}]}.
+
+%-------------------------------------------------------------------------------
+
+set_action_cache(Method, Uri, Value) ->
+    Key = atom_to_list(Method) ++ ":" ++ Uri,
+    merle:set(Key, Value).
+
+get_action_cache(Method, Uri) ->
+    Key = atom_to_list(Method) ++ ":" ++ Uri,
+    merle:getkey(Key).
+
+delete_action_cache(Method, Uri) ->
+    Key = atom_to_list(Method) ++ ":" ++ Uri,
+    merle:getkey(Key).
+
+%-------------------------------------------------------------------------------
+% Private functions
+
+ale(Key, Value) -> erlang:put(?KEY(ale, Key), Value).
+ale(Key) -> erlang:get(?KEY(ale, Key)).
--- src/ale_yaws_mod.erl
+++ src/ale_yaws_mod.erl
@@ -1,17 +1,71 @@
+%%% start, out, and out404 are called by Yaws as configured in yaws.conf
+
 -module(ale_yaws_mod).
 
 -compile(export_all).
 
 -include("ale.hrl").
 
+-define(MAX_R, 30).
+-define(MAX_T, 60).
+
 %-------------------------------------------------------------------------------
-% Called by Yaws as configured in yaws.conf
+
+%% Called after Yaws has loaded.
+start(SC) ->
+    SC2 = set_docroot(SC),
+    start_children(SC2).
+
+out(Arg) ->
+    Uri = Arg#arg.server_path,
+    case Uri of
+        % Give Yaws a chance to server static file from "/static"
+        % See http://groups.google.com/group/nitrogenweb/browse_thread/thread/c2ce70f696b77c9e
+        % If Yaws cannot find a file at the Uri, out404 below will be called
+        "/static" ++ _ -> ale:yaws(page, Arg#arg.server_path);
+
+        _ ->
+            % Errors are not cached
+            Method = rest_method(Arg),
+            case ale_routes:route_uri(Method, Uri) of
+                no_route -> c_application:error_404(Arg, Uri);
+
+                {Controller, Action, Args} ->
+                    try
+                        handle_request1(Method, Uri, Controller, Action, Arg, Args)
+                    catch
+                        Type : Reason ->
+                            error_logger:error_report([
+                                {type, Type}, {reason, Reason},
+                                {trace, erlang:get_stacktrace()}
+                            ]),
+
+                            % Type and Reason are more convenient than those
+                            % arguments of Yaws' errormod_crash
+                            c_application:error_500(Type, Reason),
+                            case ale:view() of
+                                undefined -> ok;
+                                View      -> ale:yaws(ehtml, View:render())
+                            end
+                    end
+            end
+    end,
+    ale:yaws().
+
+out404(Arg, _GC, _SC) ->
+    Uri = Arg#arg.appmoddata,
+    c_application:error_404(Uri),
+    case ale:view() of
+        undefined -> ok;
+        View      -> ale:yaws(ehtml, View:render())
+    end,
+    ale:yaws().
+
+%-------------------------------------------------------------------------------
 
 %% Modifies the current SC to set docroot and xtra_docroots to the list of all
 %% public directories in the application.
-%%
-%% Called after Yaws has loaded.
-start(SC) ->
+set_docroot(SC) ->
     {ok, GC, [SCs]} = yaws_api:getconf(),  % The 3rd element is array of array
 
     % docroot is undefined at this moment (see yaws.conf)
@@ -23,80 +77,7 @@ start(SC) ->
     SCs2 = lists:keyreplace(SC#sconf.servername, 10, SCs, SC2),
 
     yaws_api:setconf(GC, [SCs2]),
-
-    c_application:start(SC2).
-
-out(Arg) ->
-    Uri = Arg#arg.appmoddata,
-    case Uri of
-        % Give Yaws a chance to server static file from "/static"
-        % See http://groups.google.com/group/nitrogenweb/browse_thread/thread/c2ce70f696b77c9e
-        % If Yaws cannot find a file at the Uri, out404 below will be called
-        "static" ++ _ -> {page, Arg#arg.server_path};
-
-        _ ->
-            RestMethod = rest_method(Arg),
-            try ale_routes:route_uri(RestMethod, Uri) of
-                no_route ->
-                    c_application:error_404(Arg, Uri);
-
-                {Controller, Action, Args} ->
-                    Args2 = [Arg | Args],
-
-                    Halted = lists:any(
-                        fun(Module) ->
-                            case erlang:function_exported(Module, before_filter, 3) of
-                                true  -> Module:before_filter(Controller, Action, Args2);
-                                false -> false
-                            end
-                        end,
-                        [c_application, Controller]
-                    ),
-
-                    case Halted of
-                        true -> ok;
-
-                        false ->
-                            View = controller_to_view(Controller, Action),
-                            ale:put(ale, view, View),
-
-                            apply(Controller, Action, Args2),
-
-                            case ale:get(ale, view) of
-                                undefined -> ok;    % Layout is not called if there is no view
-
-                                View2 ->
-                                    Content = View2:render(),
-                                    case ale:get(ale, layout) of
-                                        undefined -> ale:put(yaws, ehtml, Content);
-
-                                        Layout ->
-                                            ale:put(ale, content_for_layout, Content),
-                                            Content2 = Layout:render(),
-                                            ale:put(yaws, ehtml, Content2)
-                                    end
-                            end
-                    end
-            catch
-                Type : Reason ->
-                    error_logger:error_report([
-                        {type, Type}, {reason, Reason},
-                        {trace, erlang:get_stacktrace()}
-                    ]),
-
-                    % Type and Reason arguments are more convenient than those
-                    % of Yaws' errormod_crash
-                    c_application:error_500(Arg, Type, Reason)
-            end,
-            ale:get(yaws)
-    end.
-
-out404(Arg, _GC, _SC) ->
-    Uri = Arg#arg.appmoddata,
-    c_application:error_404(Arg, Uri),
-    ale:get(yaws).
-
-%-------------------------------------------------------------------------------
+    SC2.
 
 xtra_docroots() ->
     xtra_docroots(filelib:wildcard("./*"), []).
@@ -121,6 +102,41 @@ xtra_docroots(Dirs, Acc) ->
 
 %-------------------------------------------------------------------------------
 
+%% Starts the application supervisor and Merle. They are supervised by Ale, which
+%% is in turn supervised by yaws_sup. We cannot let the 2 children supervised
+%% directly by yaws_sup because yaws_sup is configured as "one_for_all", thus
+%% Yaws is stopped if any of the children dies.
+start_children(SC) ->
+    ChildSpec = {
+        ale, {supervisor, start_link, [?MODULE, SC]},
+        permanent, infinity, supervisor, [ale_yaws_mod]
+    },
+    supervisor:start_child(yaws_sup, ChildSpec).
+
+%% Called by start_children above.
+init(SC) ->
+    AppSpec = {
+        ale_application, {c_application, start, [SC]},
+        permanent, brutal_kill, worker, [c_application]
+    },
+
+    ChildSpecs = case proplists:get_value("memcached", SC#sconf.opaque) of
+        undefined -> [AppSpec];
+
+        HostPort ->
+            [Host, Port] = string:tokens(HostPort, ":"),
+            Port2 = list_to_integer(Port),
+            MerleSpec = {
+                merle, {merle, connect, [Host, Port2]},
+                permanent, brutal_kill, worker, [merle]
+            },
+            [AppSpec, MerleSpec]
+    end,
+
+    {ok, {{one_for_one, ?MAX_R, ?MAX_T}, ChildSpecs}}.
+
+%-------------------------------------------------------------------------------
+
 %% 'GET'                       -> get
 %% 'POST'                      -> post
 %% 'POST' & _method = "put"    -> put
@@ -138,7 +154,79 @@ rest_method(Arg) ->
             end
     end.
 
-%-------------------------------------------------------------------------------
+handle_request1(Method, Uri, Controller, Action, Arg, Args) ->
+    PageCached = page_cached(Controller, Action),
+    case PageCached of
+        true ->
+            case merle:getkey(Uri) of
+                undefined ->
+                    error_logger:info_msg("Cache Miss: ~s", [Uri]),
+                    ale:ale(page_cached, PageCached),
+                    handle_request2(Method, Uri, Controller, Action, Arg, Args);
+
+                Html ->
+                    error_logger:info_msg("Cache Hit: ~s", [Uri]),
+                    ale:yaws(html, Html)
+            end;
+
+        false ->
+            ale:ale(page_cached, PageCached),
+            handle_request2(Method, Uri, Controller, Action, Arg, Args)
+    end.
+
+handle_request2(Method, Uri, Controller, Action, Arg, Args) ->
+    case run_before_filters(Controller, Action, Arg, Args) of
+        true -> ok;
+
+        false ->
+            View = controller_to_view(Controller, Action),
+            ale:view(View),
+
+            apply(Controller, Action, [Arg | Args]),
+
+            case ale:view() of
+                undefined -> ok;    % Layout is not called if there is no view
+
+                View2 ->
+                    Content = View2:render(),
+                    Content2 = case ale:layout() of
+                        undefined -> Content;
+
+                        Layout ->
+                            ale:content_for_layout(Content),
+                            Layout:render()
+                    end,
+
+                    Html = yaws_api:ehtml_expand(Content2),
+                    case ale:ale(page_cached) of
+                        true ->
+                            error_logger:info_msg("Cache Write: ~s", [Uri]),
+                            merle:set(Uri, Html);
+
+                        false -> ok
+                    end,
+                    ale:yaws(html, Html)
+            end
+    end.
+
+page_cached(Controller, Action) ->
+    code:ensure_loaded(Controller),
+    case erlang:function_exported(Controller, cached_pages, 0) of
+        true  -> lists:member(Action, Controller:cached_pages());
+        false -> false
+    end.
+
+%% Returns true if the action should be halted.
+run_before_filters(Controller, Action, Arg, Args) ->
+    lists:any(
+        fun(Module) ->
+            case erlang:function_exported(Module, before_filter, 4) of
+                true  -> Module:before_filter(Controller, Action, Arg, Args);
+                false -> false
+            end
+        end,
+        [c_application, Controller]
+    ).
 
 %% Converts c_hello and show to v_hello_show
 controller_to_view(Controller, Action) ->
