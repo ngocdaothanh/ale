d07da3a5cccf7526702a3db18cb9261089dd7e01
Ngoc Dao <ngocdaothanh@gmail.com>
2009-08-13 08:00:02 +0900

    Add binding feature to i18n

--- include/ale.hrl
+++ include/ale.hrl
@@ -5,6 +5,7 @@
 
 -define(T(S), ale_i18n:t(S)).                              % i18n
 -define(TF(S, Data), io_lib:format(ale_i18n:t(S), Data)).  % i18n with format
+-define(TFB(S, Binding), ale_i18n:tfb(S, Binding)).        % i18n with binding
 
 % Applications are run in ALE_ROOT/nodes/nx
 -define(ALE_ROOT, "../..").
--- src/ale_i18n.erl
+++ src/ale_i18n.erl
@@ -4,3 +4,13 @@
   
 t(Key) ->
     Key.
+
+tfb(S, Binding) ->
+    lists:foldl(
+        fun({K, V}, Acc) ->
+            K2 = [":", atom_to_list(K)],
+            re:replace(Acc, K2, V, [global, {return, list}])
+        end,
+        t(S),
+        Binding
+    ).
