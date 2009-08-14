e8bf5db64ddaf9f3b00565fcdf322dee7271a842
Ngoc Dao <ngocdaothanh@gmail.com>
2009-08-15 06:05:23 +0900

    Remove nodes for simplicity

--- include/ale.hrl
+++ include/ale.hrl
@@ -7,5 +7,5 @@
 -define(TF(S, Data), io_lib:format(ale_i18n:t(S), Data)).  % i18n with format
 -define(TFB(S, Binding), ale_i18n:tfb(S, Binding)).        % i18n with binding
 
-% Applications are run in ALE_ROOT/nodes/nx
--define(ALE_ROOT, "../..").
+% TODO: Applications are run in ALE_ROOT/nodes/nx
+-define(ALE_ROOT, ".").
